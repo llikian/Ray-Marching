3f38c9b31c0c04ba312c250d24ed069ac342878c
llikian <pokillian64@gmail.com>
2024-08-17 09:42:08 +0200

    Removed unused line from gitignore

---
+++ build/CMakeFiles/Ray-Marching.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "/home/llikian/dev/C++/Ray-Marching/bin/Ray-Marching"
+  "/home/llikian/dev/C++/Ray-Marching/bin/Ray-Marching.pdb"
+  "CMakeFiles/Ray-Marching.dir/lib/glad/src/glad.c.o"
+  "CMakeFiles/Ray-Marching.dir/lib/glad/src/glad.c.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/Application.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/Application.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/Camera.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/Shader.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/callbacks.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/callbacks.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/main.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/main.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/Matrix4.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/Matrix4.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/geometry.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/geometry.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/transformations.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/transformations.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/trigonometry.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/trigonometry.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec2.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec2.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec3.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec3.cpp.o.d"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec4.cpp.o"
+  "CMakeFiles/Ray-Marching.dir/src/maths/vec4.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Ray-Marching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Ray-Marching.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Ray-Marching.
+# This may be replaced when dependencies are built.
