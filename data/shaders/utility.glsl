92384cd7607b7f2e9e76ed18fa165c25fae61161
llikian <pokillian64@gmail.com>
2024-08-18 10:12:22 +0200

    Added a function to color a plane into a checker

--- data/shaders/utility.glsl
+++ data/shaders/utility.glsl
@@ -47,3 +47,7 @@ vec4 sDifferenceSDF(vec4 a, vec4 b, in float t) {
     float h = clamp(0.5f - 0.5f * (a.w + b.w) / t, 0.0f, 1.0f);
     return vec4(mix(a.rgb, b.rgb, h), mix(a.w, -b.w, h) + t * h * (1.0f - h));
 }
+
+vec3 checker(in vec3 pos, in vec3 col1, in vec3 col2) {
+    return mod(floor(pos.x) + floor(pos.z), 2.0f) == 0.0f ? col1 : col2;
+}
