ac2cc9a7afcc4e14fe72c7eba915224f9f652b74
llikian <pokillian64@gmail.com>
2024-08-19 11:05:03 +0200

    Added anti-aliasing and moved render functions into their own file

--- data/shaders/default.frag
+++ data/shaders/default.frag
@@ -18,31 +18,11 @@ uniform vec3 cameraUp;
 uniform uint active_scene;
 uniform bool hasLighting;
 
-//const vec3 LIGHT_POSITION = 10.0f * vec3(cos(time), 1.0f, sin(time));
+//const vec3 LIGHT_POSITION = 30.0f * vec3(cos(time), 1.0f, sin(time));
 const vec3 LIGHT_POSITION = 30.0f * vec3(2.5f, 7.5f, 2.5f);
 
-#include "raymarching.glsl"
-#include "lighting.glsl"
-
-void render(inout vec3 color, in vec2 uv) {
-    Ray ray = Ray(cameraPos, normalize(cameraFront + uv.x * cameraRight + uv.y * cameraUp));
-    float distance = raymarch(ray, color);
-
-    const vec3 background = vec3(0.125f, 0.5f, 0.8f);
-
-    if(distance < MAX_DISTANCE) {
-        if(hasLighting) {
-            color *= phongLighting(ray, ray.origin + ray.direction * distance);
-            color = mix(background, color, exp(-0.00002f * distance * distance)); // fog
-        } else {
-            color *= vec3(0.15f * distance);
-        }
-    } else {
-        color = background + max(0.75f * ray.direction.y, 0.0f);
-    }
-}
+#include "render.glsl"
 
 void main() {
-    fragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-    render(fragColor.rgb, (gl_FragCoord.xy * 2.0f - resolution) / resolution.y);
+    fragColor = vec4(renderAntiAliasing4(), 1.0f);
 }
---
+++ data/shaders/render.glsl
@@ -0,0 +1,39 @@
+/***************************************************************************************************
+ * @file  render.frag
+ * @brief Implementation of functions regarding rendering
+ **************************************************************************************************/
+
+#include "raymarching.glsl"
+#include "lighting.glsl"
+
+vec2 getUV(in vec2 offset) {
+    return (2.0f * (gl_FragCoord.xy + offset) - resolution) / resolution.y;
+}
+
+vec3 render(in vec2 uvOffset) {
+    vec3 color = vec3(0.0f);
+
+    vec2 uv = getUV(uvOffset);
+    Ray ray = Ray(cameraPos, normalize(cameraFront + uv.x * cameraRight + uv.y * cameraUp));
+    float distance = raymarch(ray, color);
+
+    const vec3 background = vec3(0.125f, 0.5f, 0.8f);
+
+    if(distance < MAX_DISTANCE) {
+        if(hasLighting) {
+            color *= phongLighting(ray, ray.origin + ray.direction * distance);
+            color = mix(background, color, exp(-0.00002f * distance * distance)); // fog
+        } else {
+            color *= vec3(0.15f * distance);
+        }
+    } else {
+        color = background + max(0.75f * ray.direction.y, 0.0f);
+    }
+
+    return color;
+}
+
+vec3 renderAntiAliasing4() {
+    vec4 e = vec4(0.125f, -0.125f, 0.375f, -0.375f);
+    return 0.25f * (render(e.xz) + render(e.yw) + render(e.wx) + render(e.zy));
+}
